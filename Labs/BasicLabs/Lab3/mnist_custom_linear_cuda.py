6340f11ee5681e59fc883a8ea8e2bb71ecb44161
TobeyQin <paula_qin@hotmail.com>
2020-12-24 17:20:03 +0800

    add file header

--- Labs/BasicLabs/Lab3/mnist_custom_linear_cuda.py
+++ Labs/BasicLabs/Lab3/mnist_custom_linear_cuda.py
@@ -28,6 +28,8 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# This file has been changed for education and teaching purpose
+
 from __future__ import print_function
 import argparse
 import torch
